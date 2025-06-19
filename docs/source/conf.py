eac08a7682d0b55901b14210e7ec7d3bf75d618b
Surabhi Ranavat <103248739+surabhiranavat@users.noreply.github.com>
2025-06-19 12:06:16 +0100

    Update conf.py
    title and name change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Viking docs for aDNA analysis'
+author = 'Surabhi Ranavat'
 
 release = '0.1'
 version = '0.1.0'
